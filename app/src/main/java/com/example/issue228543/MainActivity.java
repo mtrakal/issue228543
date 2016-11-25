d8850746819e4e2cdafc7a1f8ed9ad908c0024aa
Matěj Trakal <mtrakal@gmail.com>
2016-11-25 16:57:39 +0100

    Reproduce step for https://code.google.com/p/android/issues/detail?id=228543

--- app/src/main/java/com/example/issue228543/MainActivity.java
+++ app/src/main/java/com/example/issue228543/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.issue228543;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SearchView mSearchMenuView;
+    private MenuItem mSearchMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        mSearchMenuView = injectSearchView(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search_ok:
+                Toast.makeText(this, "search with tooltip", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_search_fail:
+                // never called... :/
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Nullable
+    private SearchView injectSearchView(Menu menu) {
+        MenuItem searchItem = menu.findItem(R.id.action_search_fail);
+        SearchManager searchManager = (SearchManager) this.getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = null;
+        if (searchItem != null) {
+            mSearchMenuItem = searchItem;
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setOnSearchClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "!!!search withotu TOOLTIP!!!", Toast.LENGTH_LONG).show();
+                    // ... search callback...
+                }
+            });
+            searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+                @Override
+                public boolean onClose() {
+                    // close callback...
+                    return false;
+                }
+            });
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(this.getComponentName()));
+            searchView.setQueryHint("HINT");
+            searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+            return searchView;
+        }
+        return null;
     }
 }
